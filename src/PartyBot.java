6fd10af9bbaf5943e0a69b49b35a0f6cf2c6a5cd
Jaeger87 <bad_hunter_666@yahoo.it>
2018-01-25 13:30:03 +0100

    More zip files

--- src/PartyBot.java
+++ src/PartyBot.java
@@ -1,8 +1,6 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import com.botticelli.bot.Bot;
 import com.botticelli.bot.request.methods.AudioReferenceToSend;
@@ -40,33 +39,28 @@ import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
 
-
-public class PartyBot extends Bot{
+public class PartyBot extends Bot {
 
 	private long boss;
 	private ReplyKeyboardMarkupWithButtons mainMenu;
-	private boolean active = true; 
+	private boolean active = true;
 	private HashSet<Long> users;
 	private HashMap<Long, Integer> banRegister;
 	private HashSet<Long> banned;
 	private final int banLimit = 5;
-	
-	
+
 	public PartyBot(String token) throws FileNotFoundException {
 		super(token);
-
-		System.out.println(executeCommand(new ProcessBuilder("/bin/bash " +"-c "+ "/bin/echo "+ "dio")));
 		banRegister = new HashMap<>();
 		banned = new HashSet<>();
 		users = new HashSet<>();
-		
-		try (Scanner s = new Scanner(new File(Main.filePath + Constants.AUTHORIZEDUSERS)))
-		{
+
+		try (Scanner s = new Scanner(new File(Main.filePath + Constants.AUTHORIZEDUSERS))) {
 			while (s.hasNext())
 				boss = s.nextLong();
 		}
-		
-		//creating the keyboard for the menu
+
+		// creating the keyboard for the menu
 		List<List<KeyboardButton>> keyboard = new ArrayList<List<KeyboardButton>>();
 		List<KeyboardButton> line = new ArrayList<>();
 		line.add(new KeyboardButton(Constants.PREVTRACK));
@@ -75,272 +69,266 @@ public class PartyBot extends Bot{
 		line.add(new KeyboardButton(Constants.STOP));
 		line.add(new KeyboardButton(Constants.NEXTTRACK));
 		keyboard.add(line);
-		
+
 		line = new ArrayList<>();
 		line.add(new KeyboardButton(Constants.VOLDOWN));
 		line.add(new KeyboardButton(Constants.VOLUP));
 		keyboard.add(line);
-		
+
 		mainMenu = new ReplyKeyboardMarkupWithButtons(keyboard);
 		mainMenu.setResizeKeyboard(true);
-		
+
 	}
 
 	@Override
 	public void audioMessage(Message m) {
 		users.add(m.getFrom().getId());
-		if(!control(m))
+		if (!control(m))
 			return;
-		
-		if(m.getFrom().getId() == boss)
-		{
+
+		if (m.getFrom().getId() == boss) {
 			addTrack(m.getAudio());
 			return;
 		}
-		
 
-		List<List<InlineKeyboardButton>> inlKeyboard = new ArrayList<List<InlineKeyboardButton>>();
-		List<InlineKeyboardButton> line = new ArrayList<>();
-		InlineKeyboardButton button = new InlineKeyboardButton(Constants.YES);
-		button.setCallback_data(CallBackCodes.MUSICYES.toString() + Constants.SEPARATOR);
-		line.add(button);
-		
-		button = new InlineKeyboardButton(Constants.NO);
-		button.setCallback_data(CallBackCodes.MUSICNO.toString() + Constants.SEPARATOR +	m.getFrom().getId() );
-		line.add(button);
-		
-		inlKeyboard.add(line);
-		
-		AudioReferenceToSend arts = new AudioReferenceToSend(boss, m.getAudio().getFileID());
-		arts.setReplyMarkup(new InlineKeyboardMarkup(inlKeyboard));
-		arts.setCaption(captionFactory(m.getFrom()));
-		
-		sendAudiobyReference(arts);
-		
+		addTrack(m.getAudio());
+
+		/*
+		 * List<List<InlineKeyboardButton>> inlKeyboard = new
+		 * ArrayList<List<InlineKeyboardButton>>(); List<InlineKeyboardButton> line =
+		 * new ArrayList<>(); InlineKeyboardButton button = new
+		 * InlineKeyboardButton(Constants.YES);
+		 * button.setCallback_data(CallBackCodes.MUSICYES.toString() +
+		 * Constants.SEPARATOR); line.add(button);
+		 * 
+		 * button = new InlineKeyboardButton(Constants.NO);
+		 * button.setCallback_data(CallBackCodes.MUSICNO.toString() +
+		 * Constants.SEPARATOR + m.getFrom().getId() ); line.add(button);
+		 * 
+		 * inlKeyboard.add(line);
+		 * 
+		 * AudioReferenceToSend arts = new AudioReferenceToSend(boss,
+		 * m.getAudio().getFileID()); arts.setReplyMarkup(new
+		 * InlineKeyboardMarkup(inlKeyboard));
+		 * arts.setCaption(captionFactory(m.getFrom()));
+		 * 
+		 * sendAudiobyReference(arts);
+		 * 
+		 */
+
 	}
 
 	@Override
 	public void callback_query(CallbackQuery c) {
-		
-		if(!active)
+
+		if (!active)
 			return;
 
 		String[] values = c.getData().split(Constants.SEPARATOR);
 		CallBackCodes cbc = CallBackCodes.fromString(values[0]);
-		
-		switch(cbc)
-		{
+
+		switch (cbc) {
 		case PHOTOYES:
 			downloadPhotos(c.getMessage().getPhoto());
 			break;
 		case PHOTONO:
 			updateBanRegister(Long.parseLong(values[1]));
 			break;
-        case MUSICYES:
-        	addTrack(c.getMessage().getAudio());
+		case MUSICYES:
+			addTrack(c.getMessage().getAudio());
 			break;
-        case MUSICNO:
-        	updateBanRegister(Long.parseLong(values[1]));
-	        break;
+		case MUSICNO:
+			updateBanRegister(Long.parseLong(values[1]));
+			break;
 		}
-		
-		
+
 	}
 
 	@Override
 	public void chose_inline_result(ChosenInlineResult arg0) {
-		
+
 	}
 
 	@Override
 	public void contactMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void documentMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void gameMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void groupChatCreatedMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void groupChatPhotoDeleteMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void inLineQuery(InlineQuery arg0) {
-		
+
 	}
 
 	@Override
 	public void invoiceMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void leftChatMemberMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void locationMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void newChatMemberMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void newChatMembersMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void newChatPhotoMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void newChatTitleMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void photoMessage(Message m) {
-		
+
 		users.add(m.getFrom().getId());
-		if(!control(m))
+		if (!control(m))
 			return;
-		if(m.getFrom().getId() == boss)
-		{
+		if (m.getFrom().getId() == boss) {
 			downloadPhotos(m.getPhoto());
 			return;
 		}
-		
-		List<List<InlineKeyboardButton>> inlKeyboard = new ArrayList<List<InlineKeyboardButton>>();
-		List<InlineKeyboardButton> line = new ArrayList<>();
-		InlineKeyboardButton button = new InlineKeyboardButton(Constants.YES);
-		button.setCallback_data(CallBackCodes.PHOTOYES.toString() + Constants.SEPARATOR);
-		line.add(button);
-		
-		button = new InlineKeyboardButton(Constants.NO);
-		button.setCallback_data(CallBackCodes.PHOTONO.toString() + Constants.SEPARATOR +	m.getFrom().getId() );
-		line.add(button);
-		
-		inlKeyboard.add(line);
-		
-		String bigPhotoID = m.getPhoto()
-				.stream()
-				.reduce(m.getPhoto().get(0), (p1,p2) -> {if(p1.getfileSize() > p2.getfileSize()) return p1; else return p2;})
-				.getFileID();
-		
-		PhotoReferenceToSend prts = new PhotoReferenceToSend(boss, bigPhotoID);
-		prts.setReplyMarkup(new InlineKeyboardMarkup(inlKeyboard));
-		prts.setCaption(captionFactory(m.getFrom()));
-		
-		sendPhotobyReference(prts);
+
+		downloadPhotos(m.getPhoto());
+		/*
+		 * List<List<InlineKeyboardButton>> inlKeyboard = new
+		 * ArrayList<List<InlineKeyboardButton>>(); List<InlineKeyboardButton> line =
+		 * new ArrayList<>(); InlineKeyboardButton button = new
+		 * InlineKeyboardButton(Constants.YES);
+		 * button.setCallback_data(CallBackCodes.PHOTOYES.toString() +
+		 * Constants.SEPARATOR); line.add(button);
+		 * 
+		 * button = new InlineKeyboardButton(Constants.NO);
+		 * button.setCallback_data(CallBackCodes.PHOTONO.toString() +
+		 * Constants.SEPARATOR + m.getFrom().getId() ); line.add(button);
+		 * 
+		 * inlKeyboard.add(line);
+		 * 
+		 * String bigPhotoID = m.getPhoto() .stream() .reduce(m.getPhoto().get(0),
+		 * (p1,p2) -> {if(p1.getfileSize() > p2.getfileSize()) return p1; else return
+		 * p2;}) .getFileID();
+		 * 
+		 * PhotoReferenceToSend prts = new PhotoReferenceToSend(boss, bigPhotoID);
+		 * prts.setReplyMarkup(new InlineKeyboardMarkup(inlKeyboard));
+		 * prts.setCaption(captionFactory(m.getFrom()));
+		 * 
+		 * sendPhotobyReference(prts);
+		 * 
+		 */
 	}
 
 	@Override
 	public void pinnedMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void preCheckOutQueryMessage(PreCheckoutQuery arg0) {
-		
+
 	}
 
 	@Override
 	public void shippingQueryMessage(ShippingQuery arg0) {
-		
+
 	}
 
 	@Override
 	public void stickerMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void successfulPaymentMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void textMessage(Message m) {
 
 		users.add(m.getFrom().getId());
-		
-		if(!control(m))
+
+		if (!control(m))
 			return;
-		
-		
-		if (boss != m.getFrom().getId()) 
+
+		if (boss != m.getFrom().getId())
 			return;
-		
-		
-		if(m.getText().equals(Constants.STOPPARTY))
-		{
+
+		if (m.getText().equals(Constants.STOPPARTY)) {
 			users.remove(boss);
 			active = false;
-		//	vlcCommand("/STOP");
+			// vlcCommand("/STOP");
 			zipPhotos();
 			mosaic();
 			return;
 		}
-		
-		if(m.getText().equals(Constants.PREVTRACK))
-		{
+
+		if (m.getText().equals(Constants.PREVTRACK)) {
 			vlcCommand("/PREV");
 			return;
 		}
-		
-		if(m.getText().equals(Constants.PLAY))
-		{
+
+		if (m.getText().equals(Constants.PLAY)) {
 			vlcCommand("/PLAY");
 			return;
 		}
-		
-		
-		if(m.getText().equals(Constants.PAUSE))
-		{
+
+		if (m.getText().equals(Constants.PAUSE)) {
 			vlcCommand("/PAUSE");
 			return;
 		}
-		
-		if(m.getText().equals(Constants.STOP))
-		{
+
+		if (m.getText().equals(Constants.STOP)) {
 			vlcCommand("/STOP");
 			return;
 		}
-		
-		if(m.getText().equals(Constants.NEXTTRACK))
-		{
+
+		if (m.getText().equals(Constants.NEXTTRACK)) {
 			vlcCommand("/NEXT");
 			return;
 		}
-		
-		if(m.getText().equals(Constants.VOLDOWN))
-		{
+
+		if (m.getText().equals(Constants.VOLDOWN)) {
 			vlcCommand("/VOLDWN");
 			return;
 		}
-		
-		
-		if(m.getText().equals(Constants.VOLUP))
-		{
+
+		if (m.getText().equals(Constants.VOLUP)) {
 			vlcCommand("/VOLUP");
 			return;
 		}
@@ -353,149 +341,134 @@ public class PartyBot extends Bot{
 
 	@Override
 	public void venueMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void videoMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void videoNoteMessage(Message arg0) {
-		
+
 	}
 
 	@Override
 	public void voiceMessage(Message arg0) {
-		
+
 	}
 
-	
-	String printText = Main.filePath + "script/enqueue.sh";
-	private boolean vlcCommand(String command)
-	{
-		String c = "/bin/bash " +"-c "+ "\"/usr/bin/vlc --one-instance "+ command +"\"";
-		System.out.println(c);
-		ProcessBuilder pb = new ProcessBuilder(c);
-		System.out.println(executeCommand(pb));
-		
-		System.out.println(executeCommand(new ProcessBuilder("bash", printText, command)));
-		
-		System.out.println(pb.toString());
-		System.out.println(pb.command().toString());
-		System.out.println(executeCommand(new ProcessBuilder("/bin/bash" ,"-c", "echo ", "dio")));
-		System.out.println(new ProcessBuilder("/bin/bash" ,"-c", "\"echo", "dio\"").command().toString());
-		return true;
-	}
-	
-	private static synchronized boolean executeCommand(ProcessBuilder pb)
-	{
-		
-		try 
-		{
-			pb.inheritIO();
-			Process p = pb.start();
-			System.out.println(p.toString());
-			p.waitFor();
-			
-			BufferedReader reader = 
-	                new BufferedReader(new InputStreamReader(p.getInputStream()));
-	StringBuilder builder = new StringBuilder();
-	String line = null;
-	while ( (line = reader.readLine()) != null) {
-	   builder.append(line);
-	   builder.append(System.getProperty("line.separator"));
-	}
-	System.out.println(builder.toString());
-			
+	private boolean vlcCommand(String command) {
+		Process process;
+		try {
+			process = Runtime.getRuntime().exec("vlc --one-instance \"+ command");
+			process.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
-		catch (Exception e) 
-		{
-			System.out.println(e);
-			return false;
-		}
 		return true;
 	}
-	
-	
-	private boolean control(Message m)
-	{
+
+	private boolean control(Message m) {
 		return active && (!banned.contains(m.getFrom().getId()));
 	}
-	
-	
-	private void updateBanRegister(long evil)
-	{
-		if(!banRegister.containsKey(evil))
-		{
+
+	private void updateBanRegister(long evil) {
+		if (!banRegister.containsKey(evil)) {
 			banRegister.put(evil, 1);
 			return;
 		}
 		int newValue = banRegister.get(evil) + 1;
-		
-		if(newValue >= banLimit)
+
+		if (newValue >= banLimit)
 			banned.add(evil);
-	
-		banRegister.put(evil, newValue);	
+
+		banRegister.put(evil, newValue);
 		return;
 	}
-	
-	private void downloadPhotos(List<PhotoSize> photos)
-	{
-		String bigPhotoID = photos
-				.stream()
-				.reduce(photos.get(0), (p1,p2) -> {if(p1.getfileSize() > p2.getfileSize()) return p1; else return p2;})
-				.getFileID();
-		
-		String smallPhotoID = photos
-				.stream()
-				.reduce(photos.get(0), (p1,p2) -> {if(p1.getfileSize() < p2.getfileSize()) return p1; else return p2;})
-				.getFileID();
-		
+
+	private void downloadPhotos(List<PhotoSize> photos) {
+		String bigPhotoID = photos.stream().reduce(photos.get(0), (p1, p2) -> {
+			if (p1.getfileSize() > p2.getfileSize())
+				return p1;
+			else
+				return p2;
+		}).getFileID();
+
+		String smallPhotoID = photos.stream().reduce(photos.get(0), (p1, p2) -> {
+			if (p1.getfileSize() < p2.getfileSize())
+				return p1;
+			else
+				return p2;
+		}).getFileID();
+
 		downloadFileFromTelegramServer(bigPhotoID, Constants.PHOTOFOLDER + bigPhotoID + ".png");
 		downloadFileFromTelegramServer(smallPhotoID, Constants.TILESFOLDER + smallPhotoID + ".png");
 	}
-	
-	private void addTrack(Audio music)
-	{
+
+	private void addTrack(Audio music) {
 		File f = downloadFileFromTelegramServer(music, Constants.MUSICFOLDER + music.getFileID() + ".mp3");
 		vlcCommand("--playlist-enqueue " + f.getAbsolutePath());
 	}
-	
-	
+
 	private void zipPhotos()
 	{
-		try {
+		try
+		{
+
 			
-			Files.deleteIfExists(new File("photos.zip").toPath());
-			
-			ZipFile zipFile = new ZipFile("photos.zip");			
 			ZipParameters parameters = new ZipParameters();
 			parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
 
-			parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-			
-			zipFile.addFolder("photos", parameters);
-			
-			Message m = sendDocumentFile(new DocumentFileToSend(boss, zipFile.getFile()));
-			
-			String fileId = m.getDocument().getFileID();
-			
+			parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_ULTRA);
 
-			try {
-				TimeUnit.MILLISECONDS.sleep(800);
-			} catch (InterruptedException e) {
-				
-				e.printStackTrace();
-			}
-			
-			for(Long user : users)
+			Path source = Paths.get(Constants.PHOTOFOLDER);
+			List<File> photosFile = new ArrayList<>();
+
+			try 
 			{
-				sendDocumentbyReference(new DocumentReferenceToSend(user, fileId));
+				photosFile = Files.walk(source).filter(Files::isRegularFile).map(p -> p.toFile())
+						.collect(Collectors.toList());
+			} catch (IOException e1) 
+			{
+
+				e1.printStackTrace();
 			}
-			
+			int limit = 100;
+			int zipFileNumber = 0;
+			int i = 0;
+			while ((limit - photosFile.size()) < 100) 
+			{
+				Files.deleteIfExists(new File("photos"+zipFileNumber+".zip").toPath());
+
+				
+				ZipFile zipFile = new ZipFile("photos"+zipFileNumber+".zip");
+				
+				for (; i < limit && i < photosFile.size(); i++) 
+					zipFile.addFile(photosFile.get(i), parameters);
+				Message m = null;
+				while (m == null)
+					m = sendDocumentFile(new DocumentFileToSend(boss, zipFile.getFile()));
+
+				String fileId = m.getDocument().getFileID();
+
+				try {
+					TimeUnit.MILLISECONDS.sleep(800);
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				}
+
+				for (Long user : users) {
+					sendDocumentbyReference(new DocumentReferenceToSend(user, fileId));
+				}
+				limit += 100;
+				zipFileNumber++;
+			}
 		} 
 		
 		catch (ZipException e) 
@@ -508,73 +481,62 @@ public class PartyBot extends Bot{
 			e1.printStackTrace();
 		}
 	}
-	
-	private void mosaic()
-	{
+
+	private void mosaic() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		try {
 			Files.deleteIfExists(new File("mosaico.png").toPath());
 		} catch (IOException e2) {
-			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-		
+
 		Path source = Paths.get(Constants.TILESFOLDER);
 		try {
-			Files.walk(source).filter(Files::isRegularFile).forEach(f -> sb.append(f.toAbsolutePath().toString() + " "));
+			Files.walk(source).filter(Files::isRegularFile)
+					.forEach(f -> sb.append(f.toAbsolutePath().toString() + " "));
 		} catch (IOException e1) {
 
 			e1.printStackTrace();
 		}
-	
-		
-		try 
-		{
-			System.out.println("montage " + sb.toString() 
-					+ "-shadow  -geometry +1+1  -texture wall3.jpg  mosaico.png");
+
+		try {
 			Process process = Runtime.getRuntime()
-					.exec("montage " + sb.toString() 
-					+ "-shadow  -geometry +1+1  -texture wall3.jpg  mosaico.png");
+					.exec("montage " + sb.toString() + "-shadow  -geometry +1+1  -texture wall3.jpg  mosaico.png");
 			process.waitFor();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return;
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			TimeUnit.MILLISECONDS.sleep(1500);
 		} catch (InterruptedException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		
+
 		Message m = sendPhotoFile(new PhotoFileToSend(boss, new File("mosaico.png")));
 		String fileId = m.getDocument().getFileID();
 		try {
 			TimeUnit.MILLISECONDS.sleep(800);
 		} catch (InterruptedException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		for(Long user : users)
-		{
+
+		for (Long user : users) {
 			sendPhotobyReference(new PhotoReferenceToSend(user, fileId));
 		}
-		
+
 	}
-	
-	private String captionFactory(User u)
-	{
-		if(u.getUserName() != null)
+
+	private String captionFactory(User u) {
+		if (u.getUserName() != null)
 			return u.getUserName();
 		return u.getFirstName();
 	}
-	
+
 }
